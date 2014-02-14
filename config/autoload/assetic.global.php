0ad0566ea40a66dcb18c105f88cd4e951e49e9b7
Matt Light <matt.light@lightdatasys.com>
2014-02-13 21:29:09 -0800

    Commit the assetic binary path config

---
+++ config/autoload/assetic.global.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'assetrinc.binaries' => array(
+	'coffee'    => __DIR__ . '/../../node_modules/coffee-script/bin/coffee',
+	'uglifyJs'  => __DIR__ . '/../../node_modules/uglify-js/bin/uglifyjs',
+	'uglifyCss' => __DIR__ . '/../../node_modules/uglifycss/uglifycss',
+    ),
+);

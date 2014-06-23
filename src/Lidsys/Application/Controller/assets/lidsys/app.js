28214009a0afa2708be148a187d41e1860a0de76
Matt Light <matt.light@lightdatasys.com>
2014-06-22 22:58:05 -0700

    Move user ng controllers to their own module

--- src/Lidsys/Application/Controller/assets/lidsys/app.js
+++ src/Lidsys/Application/Controller/assets/lidsys/app.js
@@ -1,4 +1,4 @@
-var app =  angular.module('app', ['ngRoute', 'ldsFootball', 'ldsNavigation']);
+var app =  angular.module('app', ['ngRoute', 'ldsFootball', 'ldsNavigation', 'ldsUser']);
 
 app.config(['$injector', '$routeProvider', function ($injector, $routeProvider) {
     $routeProvider
@@ -7,79 +7,9 @@ app.config(['$injector', '$routeProvider', function ($injector, $routeProvider) 
             template: "Main",
             controller: "AppCtrl"
         })
-        .when('/user/login',
-        {
-            templateUrl: "/app/template/login/index.html",
-            controller: "LoginCtrl",
-            navigationLabel: "Login"
-        })
-        .when('/user/logout',
-        {
-            template: "Logging you out...",
-            controller: "LogoutCtrl",
-            navigationLabel: "Logout"
-        })
         .otherwise({
             template: "This doesn't exist!"
-        });
-}])
-
-app.factory('active', [function() {
-    return new ActiveService()
-}])
-
-app.directive('ldsAuthenticated', ['$rootScope', 'active', function ($rootScope, active) {
-    return {
-        restrict: "A",
-        link: function (scope, element, attrs) {
-            var wasOriginallyDisplayed = element.css('display')
-            $rootScope.$watch(
-                function (scope) {
-                    var expected = scope.$eval(attrs.ldsAuthenticated)
-                    return expected === active.isLoggedIn()
-                },
-                function (isAsExpected, wasAsExpected, scope) {
-                    if (!isAsExpected) {
-                        element.css('display', 'none')
-                    }
-                    else {
-                        element.css('display', wasOriginallyDisplayed)
-                    }
-                }
-            )
-        }
-    }
-}])
-
-app.directive('ldsAuthorized', ['$rootScope', 'active', function ($rootScope, active) {
-    return {
-        restrict: "A",
-        link: function (scope, element, attrs) {
-            var wasOriginallyDisplayed = element.css('display')
-            $rootScope.$watch(
-                function (scope) {
-                    return active.getUser().isAuthorized(attrs.ldsAuthorized)
-                },
-                function (isAuthorized, wasAuthorized, scope) {
-                    if (!isAuthorized) {
-                        element.css('display', 'none');
-                    }
-                    else {
-                        element.css('display', wasOriginallyDisplayed)
-                    }
-                }
-            )
-        }
-    }
-}])
-
-app.directive('ldsUserInfo', ['$rootScope', 'active', function ($rootScope, active) {
-    return {
-        restrict: "A",
-        link: function (scope, element, attrs) {
-            scope.user = active.getUser()
-        }
-    }
+        })
 }])
 
 app.directive('dropdownParent', ['$rootScope', 'active', function ($rootScope, active) {
@@ -106,81 +36,6 @@ app.controller('AppCtrl', ['$scope', '$http', 'active', function ($scope, $http,
             if (data.authenticated_user) {
                 active.getUser().setFromApi(data.authenticated_user)
             }
-        })
-        .error(function (data) {
-        })
-}])
-
-app.controller('LoginCtrl', ['$scope', '$location', '$http', '$window', 'active', function ($scope, $location, $http, $window, active) {
-    $scope.formChanged = function ($event) {
-        var login = $scope.login;
-
-        if (login.username != login.submittedUsername ||
-            login.password != login.submittedPassword
-        ) {
-            login.error = '';
-        }
-    }
-    $scope.processLogin = function ($event) {
-        var login = $scope.login;
-
-        login.error             = {};
-        login.submittedUsername = login.username;
-        login.submittedPassword = login.password;
-
-        if (!login.username) {
-            login.error.hasError = true;
-            login.error.username = 'Please enter your username.';
-        }
-        if (!login.password) {
-            login.error.hasError = true;
-            login.error.password = 'Please enter your password.';
-        }
-
-        if (login.error.hasError) {
-            return false;
-        }
-
-        var postData = {
-            username: login.username,
-            password: login.password
-        }
-
-        $http.post("/app/user/login/", postData)
-            .success(function (data) {
-                if (data.authenticated_user) {
-                    active.getUser().setFromApi(data.authenticated_user)
-                    login.error.form = 'Success!!';
-                    $window.history.back()
-                }
-                else {
-                    login.error.form = 'The provided username/password are incorrect.';
-                }
-            })
-            .error(function (data) {
-                login.error.form = 'There was an error processing your login request. Please contact an administrator.';
-            })
-
-        return false;
-    }
-
-    $scope.login = {
-        submitEnabled: false,
-        error: {},
-        username: '',
-        password: '',
-        previousUsername: '',
-        previousPassword: ''
-    }
-}])
-
-app.controller('LogoutCtrl', ['$scope', '$location', '$http', '$window', 'active', function ($scope, $location, $http, $window, active) {
-    $http.post("/app/user/logout/")
-        .success(function (data) {
-            if (data.logged_out) {
-                active.getUser().setFromApi({})
-            }
-            $window.history.back()
         })
         .error(function (data) {
         })

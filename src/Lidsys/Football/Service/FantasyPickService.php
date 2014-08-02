a6ce5621fffc0a0511a93c80dc0f9a08600bebe2
Matt Light <matt.light@lightdatasys.com>
2014-08-02 01:56:19 -0700

    Add savePicks() method to football service

--- src/Lidsys/Football/Service/FantasyPickService.php
+++ src/Lidsys/Football/Service/FantasyPickService.php
@@ -66,4 +66,91 @@ class FantasyPickService
 
         return $picks;
     }
+
+    public function savePicks($user_id, array $picks)
+    {
+        $game_ids     = array_keys($picks);
+        $game_ids_sql = implode(',', array_map('intval', $game_ids));
+
+        $valid_game_ids = array();
+
+        $db    = $this->db;
+
+        $query = $db->query(
+            "
+                SELECT
+                    playerId AS player_id
+                FROM player_user
+                WHERE userId = :user_id
+            ",
+            array(
+                'user_id' => $user_id,
+            )
+        );
+        $player    = $query->fetch();
+        $player_id = $player['player_id'];
+
+        $query = $db->query(
+            "
+                SELECT
+                    gameId AS game_id
+                FROM nflGame game
+                WHERE gameId IN (:game_ids)
+                    AND gameTime < :now
+            ",
+            array(
+                'game_ids' => $game_ids_sql,
+                'now'      => gmdate('Y-m-d H:i:s'),
+            )
+        );
+        while ($game = $query->fetch()) {
+            $valid_game_ids[] = $game['game_id'];
+        }
+        $db->query(
+            "
+                DELETE FROM nflFantPick
+                WHERE gameId IN (:game_ids)
+                    AND playerId = :player_id
+            ",
+            array(
+                'game_ids'  => implode(',', $valid_game_ids),
+                'player_id' => $player_id,
+            )
+        );
+
+        $row_num    = 0;
+        $value_sets = array();
+        $values     = array(
+            'player_id' => $player_id,
+        );
+        foreach ($valid_game_ids as $valid_game_id) {
+            $value_sets[] = "
+                (
+                    :player_id,
+                    :game_id_{$row_num},
+                    :team_id_{$row_num}
+                )
+            ";
+            $values["game_id_{$row_num}"] = $valid_game_id;
+            $values["team_id_{$row_num}"] = $picks[$valid_game_id];
+            ++$row_num;
+        }
+
+        if (count($value_sets)) {
+            $values_sql = implode(",\n", $value_sets);
+            $db->query(
+                "
+                    INSERT INTO nflFantPick
+                    (
+                        playerId,
+                        gameId,
+                        teamId
+                    )
+                    VALUES
+                    {$values_sql}
+                ",
+                $values
+            );
+        }
+    }
 }

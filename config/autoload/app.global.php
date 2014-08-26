9fc61d34d0009660843bc33cbdae0dc528f5d9fb
Matt Light <matt.light@lightdatasys.com>
2014-08-25 23:22:47 -0700

    Add the ability to configure commissioner name/email

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -2,5 +2,9 @@
 return array(
     'app' => array(
         'base_url' => 'http://lightdatasys.com/app/#',
+        'commissioner' => array(
+            'name' => 'The Commissioner',
+            'email' => '',
+        ),
     ),
 );

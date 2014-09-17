a9043aee7994ef3046fd2f7f21a5849da5f3fb80
Matt Light <matt.light@lightdatasys.com>
2014-09-17 08:20:33 -0700

    Reduce football.js line lengths

--- src/Lidsys/Application/Controller/assets/lidsys/football.js
+++ src/Lidsys/Application/Controller/assets/lidsys/football.js
@@ -6,17 +6,30 @@ module.config(['$injector', '$routeProvider', function ($injector, $routeProvide
         {
             templateUrl: "/app/template/football/my-picks.html",
             controller: "LidsysFootballPicksCtrl",
-            resolve: [['$injector', '$route', '$q', 'lidsysFootballSchedule', 'lidsysFootballTeamStanding', function($injector, $route, $q, footballSchedule, footballTeamStanding) {
-                var resolvers = $injector.get('lidsysFootballPicksRouteResolver');
-                return $q.all({
-                    resolvePicks: $injector.invoke(resolvers.resolvePicks)
-                }).then(function () {
-                    return footballTeamStanding.load(
-                        footballSchedule.getSelectedSeason().year,
-                        footballSchedule.getSelectedWeek().week_number
-                    )
-                })
-            }]],
+            resolve: [[
+                '$injector',
+                '$route',
+                '$q',
+                'lidsysFootballSchedule',
+                'lidsysFootballTeamStanding',
+                function(
+                    $injector,
+                    $route,
+                    $q,
+                    footballSchedule,
+                    footballTeamStanding
+                ) {
+                    var resolvers = $injector.get('lidsysFootballPicksRouteResolver');
+                    return $q.all({
+                        resolvePicks: $injector.invoke(resolvers.resolvePicks)
+                    }).then(function () {
+                        return footballTeamStanding.load(
+                            footballSchedule.getSelectedSeason().year,
+                            footballSchedule.getSelectedWeek().week_number
+                        )
+                    })
+                }
+            ]],
             navigationLabel: "My Picks",
             isFootball: true
         })
@@ -48,80 +61,134 @@ module.config(['$injector', '$routeProvider', function ($injector, $routeProvide
         {
             templateUrl: "/app/template/football/team-standings.html",
             controller: "LidsysFootballTeamStandingsCtrl",
-            resolve: [['$injector', '$route', '$q', 'lidsysFootballSchedule', 'lidsysFootballTeamStanding', function($injector, $route, $q, footballSchedule, footballTeamStanding) {
-                var resolvers = $injector.get('lidsysFootballWeekSensitiveRouteResolver');
-                return $q.all({
-                    resolveValidWeek: $injector.invoke(resolvers.resolveValidWeek),
-                    resolveTeams:     $injector.invoke(resolvers.resolveTeams)
-                }).then(function () {
-                    return footballTeamStanding.load(
-                        footballSchedule.getSelectedSeason().year,
-                        footballSchedule.getSelectedWeek().week_number
-                    )
-                })
-            }]],
+            resolve: [[
+                '$injector',
+                '$route',
+                '$q',
+                'lidsysFootballSchedule',
+                'lidsysFootballTeamStanding',
+                function(
+                    $injector,
+                    $route,
+                    $q,
+                    footballSchedule,
+                    footballTeamStanding
+                ) {
+                    var resolvers = $injector.get('lidsysFootballWeekSensitiveRouteResolver');
+                    return $q.all({
+                        resolveValidWeek: $injector.invoke(resolvers.resolveValidWeek),
+                        resolveTeams:     $injector.invoke(resolvers.resolveTeams)
+                    }).then(function () {
+                        return footballTeamStanding.load(
+                            footballSchedule.getSelectedSeason().year,
+                            footballSchedule.getSelectedWeek().week_number
+                        )
+                    })
+                }
+            ]],
             navigationLabel: "Team Standings",
             isFootball: true
         })
 }])
 
 module.constant('lidsysFootballWeekSensitiveRouteResolver', {
-    resolveValidWeek: ['$location', '$q', '$route', 'lidsysFootballSchedule', function ($location, $q, $route, footballSchedule) {
-        var year = $route.current.params.year,
-            week = $route.current.params.week
-        return footballSchedule.load(year, week)
-            .catch(function (message) {
-                if (message.year && message.week) {
-                    $location.path(
-                        $route.current.originalPath
-                            .replace(":year?", message.year)
-                            .replace(":week?", message.week)
-                    ).replace()
-                }
+    resolveValidWeek: [
+        '$location',
+        '$q',
+        '$route',
+        'lidsysFootballSchedule',
+        function (
+            $location,
+            $q,
+            $route,
+            footballSchedule
+        ) {
+            var year = $route.current.params.year,
+                week = $route.current.params.week
+            return footballSchedule.load(year, week)
+                .catch(function (message) {
+                    if (message.year && message.week) {
+                        $location.path(
+                            $route.current.originalPath
+                                .replace(":year?", message.year)
+                                .replace(":week?", message.week)
+                        ).replace()
+                    }
 
-                return $q.reject(message)
-            })
-    }],
+                    return $q.reject(message)
+                })
+        }
+    ],
     resolveTeams: ['lidsysFootballTeam', function (footballTeam) {
         return footballTeam.load()
     }]
 })
 
 module.constant('lidsysFootballPicksRouteResolver', {
-    resolvePicks: ['$injector', '$route', '$q', 'lidsysFootballFantasyPlayer','lidsysFootballPick',  'lidsysFootballSchedule', function($injector, $route, $q, footballFantasyPlayer, footballPick, footballSchedule) {
-        var resolvers = $injector.get('lidsysFootballWeekSensitiveRouteResolver');
-        return $q.all({
-            resolveValidWeek: $injector.invoke(resolvers.resolveValidWeek),
-            resolveTeams:     $injector.invoke(resolvers.resolveTeams)
-        }).then(function () {
-            return footballPick.load(
-                footballSchedule.getSelectedSeason().year,
-                footballSchedule.getSelectedWeek().week_number
-            )
-        }).then(function () {
-            return footballFantasyPlayer.load(
-                footballSchedule.getSelectedSeason().year
-            )
-        })
-    }]
+    resolvePicks: [
+        '$injector',
+        '$route',
+        '$q',
+        'lidsysFootballFantasyPlayer',
+        'lidsysFootballPick',
+        'lidsysFootballSchedule',
+        function(
+            $injector,
+            $route,
+            $q,
+            footballFantasyPlayer,
+            footballPick,
+            footballSchedule
+        ) {
+            var resolvers = $injector.get('lidsysFootballWeekSensitiveRouteResolver');
+            return $q.all({
+                resolveValidWeek: $injector.invoke(resolvers.resolveValidWeek),
+                resolveTeams:     $injector.invoke(resolvers.resolveTeams)
+            }).then(function () {
+                return footballPick.load(
+                    footballSchedule.getSelectedSeason().year,
+                    footballSchedule.getSelectedWeek().week_number
+                )
+            }).then(function () {
+                return footballFantasyPlayer.load(
+                    footballSchedule.getSelectedSeason().year
+                )
+            })
+        }
+    ]
 })
 
 module.constant('lidsysFootballFantasyStandingsRouteResolver', {
-    resolveFantasyStandings: ['$injector', '$route', '$q', 'lidsysFootballFantasyPlayer','lidsysFootballFantasyStanding',  'lidsysFootballSchedule', function($injector, $route, $q, footballFantasyPlayer, footballFantasyStanding, footballSchedule) {
-        var resolvers = $injector.get('lidsysFootballWeekSensitiveRouteResolver');
-        return $q.all({
-            resolveValidWeek: $injector.invoke(resolvers.resolveValidWeek),
-            resolveTeams:     $injector.invoke(resolvers.resolveTeams)
-        }).then(function () {
-            return footballFantasyPlayer.load(
-                footballSchedule.getSelectedSeason().year
-            )
-        }).then(function () {
-            return footballFantasyStanding.load(
-                footballSchedule.getSelectedSeason().year
-            )
-        })
-    }]
+    resolveFantasyStandings: [
+        '$injector',
+        '$route',
+        '$q',
+        'lidsysFootballFantasyPlayer',
+        'lidsysFootballFantasyStanding',
+        'lidsysFootballSchedule',
+        function(
+            $injector,
+            $route,
+            $q,
+            footballFantasyPlayer,
+            footballFantasyStanding,
+            footballSchedule
+        ) {
+            var resolvers = $injector.get('lidsysFootballWeekSensitiveRouteResolver');
+            return $q.all({
+                resolveValidWeek: $injector.invoke(resolvers.resolveValidWeek),
+                resolveTeams:     $injector.invoke(resolvers.resolveTeams)
+            }).then(function () {
+                return footballFantasyPlayer.load(
+                    footballSchedule.getSelectedSeason().year
+                )
+            }).then(function () {
+                return footballFantasyStanding.load(
+                    footballSchedule.getSelectedSeason().year
+                )
+            })
+        }
+    ]
 })
 
 module.factory('lidsysFootballFantasyPlayer', ['$http', '$q', function($http, $q) {
@@ -132,13 +199,33 @@ module.factory('lidsysFootballFantasyStanding', ['$http', '$q', function($http, 
     return new FootballFantasyStandingService($http, $q)
 }])
 
-module.factory('lidsysFootballPick', ['$http', '$timeout', '$q', '$window', function($http, $timeout, $q, $window) {
-    return new FootballPickService($http, $timeout, $q, $window)
-}])
+module.factory('lidsysFootballPick', [
+    '$http',
+    '$timeout',
+    '$q',
+    '$window',
+    function(
+        $http,
+        $timeout,
+        $q,
+        $window
+    ) {
+        return new FootballPickService($http, $timeout, $q, $window)
+    }
+])
 
-module.factory('lidsysFootballSchedule', ['$http', '$q', 'lidsysFootballTeam', function($http, $q, footballTeam) {
-    return new FootballScheduleService($http, $q, footballTeam)
-}])
+module.factory('lidsysFootballSchedule', [
+    '$http',
+    '$q',
+    'lidsysFootballTeam',
+    function(
+        $http,
+        $q,
+        footballTeam
+    ) {
+        return new FootballScheduleService($http, $q, footballTeam)
+    }
+])
 
 module.factory('lidsysFootballTeam', ['$http', '$q', function($http, $q) {
     return new FootballTeamService($http, $q)
@@ -151,23 +238,34 @@ module.factory('lidsysFootballTeamStanding', ['$http', '$q', function($http, $q)
 module.directive('ldsFootballWeekSelector', [function () {
     return {
         restrict: "E",
-        controller: ['$location', '$route', '$scope', 'lidsysFootballSchedule', function ($location,  $route, $scope, footballSchedule) {
-            var season = footballSchedule.getSelectedSeason(),
-                week   = footballSchedule.getSelectedWeek()
-            $scope.week_selector = {
-                season:  season,
-                week:    week,
-                seasons: footballSchedule.getSeasons(),
-                weeks:   footballSchedule.getWeeksArray(season.year)
-            };
-            $scope.changeSelectedWeek = function() {
-                $location.path(
-                    $route.current.originalPath
-                        .replace(":year?", $scope.week_selector.season.year)
-                        .replace(":week?", $scope.week_selector.week.week_number)
-                )
+        controller: [
+            '$location',
+            '$route',
+            '$scope',
+            'lidsysFootballSchedule',
+            function (
+                $location,
+                $route,
+                $scope,
+                footballSchedule
+            ) {
+                var season = footballSchedule.getSelectedSeason(),
+                    week   = footballSchedule.getSelectedWeek()
+                $scope.week_selector = {
+                    season:  season,
+                    week:    week,
+                    seasons: footballSchedule.getSeasons(),
+                    weeks:   footballSchedule.getWeeksArray(season.year)
+                };
+                $scope.changeSelectedWeek = function() {
+                    $location.path(
+                        $route.current.originalPath
+                            .replace(":year?", $scope.week_selector.season.year)
+                            .replace(":week?", $scope.week_selector.week.week_number)
+                    )
+                }
             }
-        }],
+        ],
         templateUrl: "/app/template/football/week-selector.html"
     }
 }])
@@ -615,73 +713,96 @@ module.controller('LidsysFootballFantasyStandingsCtrl', [
     }
 ])
 
-module.controller('LidsysFootballScheduleCtrl', ['$scope', 'lidsysFootballSchedule', 'lidsysFootballTeam', function ($scope, footballSchedule, footballTeam) {
-    var teams   = footballTeam.getTeams(),
-        games   = footballSchedule.getGames(),
-        game    = null,
-        game_id = null
-    $scope.games        = games
-    $scope.prevGameTime = null
-    $scope.headerExists = function (game) {
-        if ($scope.prevGameTime === game.start_time) {
-            return false
-        }
+module.controller('LidsysFootballScheduleCtrl', [
+    '$scope',
+    'lidsysFootballSchedule',
+    'lidsysFootballTeam',
+    function (
+        $scope,
+        footballSchedule,
+        footballTeam
+    ) {
+        var teams   = footballTeam.getTeams(),
+            games   = footballSchedule.getGames(),
+            game    = null,
+            game_id = null
+        $scope.games        = games
+        $scope.prevGameTime = null
+        $scope.headerExists = function (game) {
+            if ($scope.prevGameTime === game.start_time) {
+                return false
+            }
 
-        $scope.prevGameTime = game.start_time
-        return true
-    }
-}])
-
-module.directive('ldsFootballDivisionSelector', [function () {
-    return {
-        restrict: "E",
-        controller: ['$location', '$route', '$scope', 'lidsysFootballTeamStanding', function ($location,  $route, $scope, footballTeamStanding) {
-            // var conference = footballTeamStanding.getSelectedConference(),
-            //     division   = footballTeamStanding.getSelectedDivision()
-            $scope.getSelectedConference = function () {
-                return footballTeamStanding.getSelectedConference()
-            }
-            $scope.setSelectedConference = function ($event, conference) {
-                $event.preventDefault()
-                footballTeamStanding.setSelectedConference(conference)
-                $route.reload()
-            }
-            $scope.getSelectedDivision   = function () {
-                return footballTeamStanding.getSelectedDivision()
-            }
-            $scope.setSelectedDivision   = function ($event, division) {
-                $event.preventDefault()
-                footballTeamStanding.setSelectedDivision(division)
-                $route.reload()
-            }
-        }],
-        templateUrl: "/app/template/football/division-selector.html"
-    }
-}])
-
-module.controller('LidsysFootballTeamStandingsCtrl', ['$scope', 'lidsysFootballSchedule', 'lidsysFootballTeam', 'lidsysFootballTeamStanding', function ($scope, footballSchedule, footballTeam, footballTeamStanding) {
-    var season = footballSchedule.getSelectedSeason(),
-        week   = footballSchedule.getSelectedWeek(),
-        conference = footballTeamStanding.getSelectedConference(),
-        division   = footballTeamStanding.getSelectedDivision()
-    var teams               = footballTeam.getTeams(),
-        standings           = footballTeamStanding.getTeamStandings(
-            season.year,
-            week.week_number
-        ),
-        standing_idx,
-        standing,
-        team,
-        filteredStandings = []
-    for (standing_idx in standings) {
-        standing = standings[standing_idx]
-        if (standing.team_id) {
-            team          = teams[standing.team_id]
-            standing.team = team
-            if (team.conference == conference && team.division == division) {
-                filteredStandings.push(standing)
-            }
+            $scope.prevGameTime = game.start_time
+            return true
         }
     }
-    $scope.standings = filteredStandings
-}])
+])
+
+module.directive('ldsFootballDivisionSelector', [
+    function () {
+        return {
+            restrict: "E",
+            controller: [
+                '$location',
+                '$route',
+                '$scope',
+                'lidsysFootballTeamStanding',
+                function ($location,  $route, $scope, footballTeamStanding) {
+                    // var conference = footballTeamStanding.getSelectedConference(),
+                    //     division   = footballTeamStanding.getSelectedDivision()
+                    $scope.getSelectedConference = function () {
+                        return footballTeamStanding.getSelectedConference()
+                    }
+                    $scope.setSelectedConference = function ($event, conference) {
+                        $event.preventDefault()
+                        footballTeamStanding.setSelectedConference(conference)
+                        $route.reload()
+                    }
+                    $scope.getSelectedDivision   = function () {
+                        return footballTeamStanding.getSelectedDivision()
+                    }
+                    $scope.setSelectedDivision   = function ($event, division) {
+                        $event.preventDefault()
+                        footballTeamStanding.setSelectedDivision(division)
+                        $route.reload()
+                    }
+                }
+            ],
+            templateUrl: "/app/template/football/division-selector.html"
+        }
+    }
+])
+
+module.controller('LidsysFootballTeamStandingsCtrl', [
+    '$scope',
+    'lidsysFootballSchedule',
+    'lidsysFootballTeam',
+    'lidsysFootballTeamStanding',
+    function ($scope, footballSchedule, footballTeam, footballTeamStanding) {
+        var season = footballSchedule.getSelectedSeason(),
+            week   = footballSchedule.getSelectedWeek(),
+            conference = footballTeamStanding.getSelectedConference(),
+            division   = footballTeamStanding.getSelectedDivision()
+        var teams               = footballTeam.getTeams(),
+            standings           = footballTeamStanding.getTeamStandings(
+                season.year,
+                week.week_number
+            ),
+            standing_idx,
+            standing,
+            team,
+            filteredStandings = []
+        for (standing_idx in standings) {
+            standing = standings[standing_idx]
+            if (standing.team_id) {
+                team          = teams[standing.team_id]
+                standing.team = team
+                if (team.conference == conference && team.division == division) {
+                    filteredStandings.push(standing)
+                }
+            }
+        }
+        $scope.standings = filteredStandings
+    }
+])
